e56bb784e53b4102da27a9f21377ae2810707a0b
mkaa <20796385+thomasbad@users.noreply.github.com>
2022-11-15 10:45:51 +0800

    script enhancement

--- assets/customscript.js
+++ assets/customscript.js
@@ -5,13 +5,6 @@ $(function () {
   $("#footer").load("footer.html");
 });
 
-//jquery content switcher
-$("a.button").on("click", function(){
-  const switchBtn = $(this).parent().data("target");   
-    $("div[id]").addClass("is-hidden");
-    $("div").removeClass("is-hidden");
-});
-
 //Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon
 function navBarShow() {
   var x = document.getElementById("navMenu");
@@ -22,26 +15,48 @@ function navBarShow() {
   }
 }
 
+//dark mode onload checking
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  document.getElementById("checkbox").checked = true; //Switch the dark button by default if it is in dark mode
+  document.getElementById("mobileSwitch").checked = true;
+}
+
+//light mode
+function lightMode() {
+  var element = document.body;
+  element.classList.toggle("light-mode");
+}
+
+//Load Darkmode localstorage when page loaded
+function themeOnLoad() {
+  var isDarkMode = localStorage.getItem("isDarkMode");
+  if (isDarkMode == "true") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+}
+
+//dark mode button trigger
+function darkModeTrig() {
+  var element = document.body;
+  var isDarkMode = localStorage.getItem("isDarkMode");
+  element.classList.toggle("dark-mode");
+  if (isDarkMode === "true") {
+    localStorage.setItem("isDarkMode", false);
+    document.getElementById("checkbox").checked = false; //Switch the dark button by default if it is in dark mode
+    document.getElementById("mobileSwitch").checked = false;
+  } else {
+    localStorage.setItem("isDarkMode", true);
+    document.getElementById("checkbox").checked = true; //Switch the dark button by default if it is in dark mode
+    document.getElementById("mobileSwitch").checked = true; //sync both switch into same status
+  }
+}
+
 //Clipboard function
 var clipboard = new ClipboardJS('.button');
-
-//Copy success event (Not in use, for future use)
-/*clipboard.on('success', function (e) {
-  console.info('Action:', e.action);
-  console.info('Text:', e.text);
-  console.info('Trigger:', e.trigger);
-  var tooltip = document.getElementById("myTooltip");
-  tooltip.innerHTML = "Copied !&nbsp;";
-});
-
-//Copy fail event (Not in use, for future use)
-clipboard.on('error', function (e) {
-  console.info('Action:', e.action);
-  console.info('Text:', e.text);
-  console.info('Trigger:', e.trigger);
-  var tooltip = document.getElementById("myTooltip");
-  alert("Copy failed, please check your browser setting, or do it manually");
-});*/
 
 //Top Button function:
 // Get the button:
@@ -74,44 +89,4 @@ document.addEventListener('DOMContentLoaded', function () {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-//Load Darkmode localstorage when page loaded
-function themeOnLoad() {
-  var isDarkMode = localStorage.getItem("isDarkMode");
-  if (isDarkMode == "true") {
-    darkMode();
-  } else {
-    lightMode();
-  }
-}
-
-//dark mode button trigger
-function darkModeTrig() {
-  var element = document.body;
-  var isDarkMode = localStorage.getItem("isDarkMode");
-  element.classList.toggle("dark-mode");
-  if (isDarkMode === "true") {
-    localStorage.setItem("isDarkMode", false);
-    document.getElementById("checkbox").checked = false; //Switch the dark button by default if it is in dark mode
-    document.getElementById("mobileSwitch").checked = false;
-  } else {
-    localStorage.setItem("isDarkMode", true);
-    document.getElementById("checkbox").checked = true; //Switch the dark button by default if it is in dark mode
-    document.getElementById("mobileSwitch").checked = true; //sync both switch into same status
-  }
-}
-
-//dark mode onload checking
-function darkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-  document.getElementById("checkbox").checked = true; //Switch the dark button by default if it is in dark mode
-  document.getElementById("mobileSwitch").checked = true;
-}
-
-//light mode
-function lightMode() {
-  var element = document.body;
-  element.classList.toggle("light-mode");
 }
